e0c024689c0851bfb077968de2a485bc06175561
Dylan Pope <78003555+PopiWanKenobi@users.noreply.github.com>
2022-04-28 17:41:36 -0600

    Tryin peer to peer

--- gimm_310_app/ARViewController.swift
+++ gimm_310_app/ARViewController.swift
@@ -7,19 +7,21 @@
 import SwiftUI
 import ARKit
 import RealityKit
+import MultipeerConnectivity
 
 class ARViewController: UIViewController{
     
+    var multipeerSession: MultipeerSession!
     var currMdl :Entity? = nil
     let anchor = AnchorEntity()
     let thumbsUpMdl = try? Entity.load(named: "Like_Button.usdz")
     let thumbsDownMdl = try? Entity.load(named: "Dislike_Button.usdz")
     let laughingMdl = try? Entity.load(named: "LaughingEmojiModel.usdz")
+    let angryMdl = try? Entity.load(named: "AngryEmojiModel.usdz")
     let freePoop = try? Entity.load(named: "3d_Poop_Emoji.usdz")
     
     var deleted: Bool = true
     
-    let scalar = simd_make_float3(0.01, 0.01, 0.01)
     
     let defPos = simd_make_float3(0, 1, 0)
     
@@ -31,41 +33,44 @@ class ARViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        multipeerSession = multipeerSession(receivedDataHandler: receivedData)
         
     }
    
     @IBAction func LikeButton(_ sender: Any) {
         print("Like button pressed")
         LoadModel(mdl: thumbsUpMdl!)
-        deleted = false
+        
     
     }
     
     @IBAction func DislikeButton(_ sender: Any) {
         print("Dislike button pressed")
         LoadModel(mdl: thumbsDownMdl!)
-        deleted = false
+        
         
     }
     
-    @IBAction func HandButton(_ sender: Any) {
-        print("Hand button pressed")
+
+    @IBAction func HandRaise(_ sender: Any) {
+        print("I have raised my hand")
     }
     
-    
-    @IBAction func LaughingButton(_ sender: Any) {
-        print("Laughing button pressed")
+    @IBAction func LaughingerButton(_ sender: Any) {
         LoadModel(mdl: laughingMdl!)
-        deleted = false
+        
     }
     
-    @IBAction func AngryButton(_ sender: Any) {
-        print("Angry button pressed")
+    @IBAction func AngierButton(_ sender: Any) {
+        LoadModel(mdl: angryMdl!)
+        
     }
+    
     
     @IBAction func QuestionButton(_ sender: Any) {
-        print("Question button pressed")
+        print("I have a question")
     }
+    
     
     @IBAction func ClearButton(_ sender: Any) {
         if (deleted == true) {
@@ -79,11 +84,14 @@ class ARViewController: UIViewController{
     }
     
     func LoadModel (mdl : Entity) {
+        if(deleted == false){
+            anchor.removeChild(currMdl!)
+        }
         anchor.position = defPos
-        anchor.scale = scalar
         currMdl = mdl
         anchor.addChild(currMdl!)
         arView.scene.anchors.append(anchor)
+        deleted = false
 
     }
 }
